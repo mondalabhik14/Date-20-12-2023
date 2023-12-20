059dcac37b1c8a9c6eab3ebca3bde56d5c1fade2
mondalabhik14 <139350149+mondalabhik14@users.noreply.github.com>
2023-12-20 08:37:10 +0000

    small api call

---
+++ script.js
@@ -0,0 +1,25 @@
+ async function abcd(){
+    var userdata = await fetch("https://randomuser.me/api/");
+    var data = await userdata.json();
+    console.log(data.results[0].name);
+    var largeImage = data.results[0].picture.large;
+    var mediumImage = data.results[0].picture.medium;
+    var thumnilImage = data.results[0].picture.large;
+
+    // console.log(largeImage);
+    // console.log(mediumImage);
+    // console.log(thumnilImage);
+
+   var img1 = document.querySelector("#a");
+   var img2 = document.querySelector("#b");
+   var img3 = document.querySelector("#c");
+   var name = document.querySelectorAll("label");
+   name.forEach((name)=>{
+    name.innerText = `${data.results[0].name.title + " " + data.results[0].name.first + " " + data.results[0].name.last }`
+   })
+
+   img1.src = largeImage;
+   img2.src = mediumImage; 
+   img3.src = thumnilImage;
+}
+abcd();
